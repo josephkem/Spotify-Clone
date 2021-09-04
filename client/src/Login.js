abb664a2406fe862a5ef1816ed0ed848b7fa9dbc
josephkem <josephkemoy@gmail.com>
2021-09-04 00:04:42 -0400

    Created button to redirect user to login with Spotify

--- client/src/Login.js
+++ client/src/Login.js
@@ -4,5 +4,11 @@ const auth_url =
   "https://accounts.spotify.com/authorize?client_id=62eb3eb3f3204d979323b9244059aeaf&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-library-read$20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 export default function Login() {
-  return <div>Testing</div>;
+  return (
+    <Container>
+      <a href={auth_url} className="btn btn-success btn-lg">
+        Login With Spotify
+      </a>
+    </Container>
+  );
 }
