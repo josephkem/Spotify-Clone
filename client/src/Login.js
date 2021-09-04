54f781469eae543b057e363d4122de8868316927
josephkem <josephkemoy@gmail.com>
2021-09-03 23:20:27 -0400

    Created auth url to authnticate spotify

---
+++ client/src/Login.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const auth_url =
+  "https://accounts.spotify.com/authorize?client_id=62eb3eb3f3204d979323b9244059aeaf&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-library-read$20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+
+export default function Login() {
+  return <div></div>;
+}
