8ab341508c06b0aa2ba3fcbcb27a582377b1ce1c
josephkem <josephkemoy@gmail.com>
2021-09-04 00:56:33 -0400

    Initialized express and spotify web api to authorize token in the server file

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const SpotifyWebApi = require("spotify-web-api-node");
+
+const app = express();
+
+app.post("/login", (req, res) => {
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:3000",
+    clientId: "62eb3eb3f3204d979323b9244059aeaf",
+    clientSecret: e8736211e4164424afaa4f57cc129415,
+  });
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch(() => {
+      res.sendStatus(400);
+    });
+});
