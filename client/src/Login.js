e108d0972496ec5660758be27a18a9441842d384
josephkem <josephkemoy@gmail.com>
2021-09-04 00:36:46 -0400

    Fixed an error with the redirect uri

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 const auth_url =
-  "https://accounts.spotify.com/authorize?client_id=62eb3eb3f3204d979323b9244059aeaf&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-library-read$20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+  "https://accounts.spotify.com/authorize?client_id=62eb3eb3f3204d979323b9244059aeaf&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 export default function Login() {
   return (
-    <Container>
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
       <a href={auth_url} className="btn btn-success btn-lg">
         Login With Spotify
       </a>
