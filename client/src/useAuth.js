ebefa6a9546629a598fa4365b4af9debaf01f279
josephkem <josephkemoy@gmail.com>
2021-09-04 21:06:23 -0400

    Fixed an error that caused project to crash

--- client/src/useAuth.js
+++ client/src/useAuth.js
@@ -13,6 +13,9 @@ export default function useAuth(code) {
       })
       .then((res) => {
         console.log(res.data);
+      })
+      .catch(() => {
+        window.location = "/";
       });
   }, [code]);
 }
--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const SpotifyWebApi = require("spotify-web-api-node");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
-app, use(cors());
+app.use(cors());
+app.use(express.json());
 
 app.post("/login", (req, res) => {
+  const code = req.body.code;
   const spotifyApi = new SpotifyWebApi({
     redirectUri: "http://localhost:3000",
     clientId: "62eb3eb3f3204d979323b9244059aeaf",
-    clientSecret: e8736211e4164424afaa4f57cc129415,
+    clientSecret: "e8736211e4164424afaa4f57cc129415",
   });
 
   spotifyApi
@@ -21,7 +24,8 @@ app.post("/login", (req, res) => {
         expiresIn: data.body.expires_in,
       });
     })
-    .catch(() => {
+    .catch((error) => {
+      console.log(error);
       res.sendStatus(400);
     });
 });
