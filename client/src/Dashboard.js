ac6732218db6110256fbd6eb6b52403f2fecad43
josephkem <josephkemoy@gmail.com>
2021-09-05 13:12:15 -0400

    Installed Spotify-Web-API to interact with the clientside

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -6,13 +6,14 @@ export default function Dashboard({ code }) {
   const accessToken = useAuth(code);
   const [search, setSearch] = useState("");
   return (
-    <Container>
+    <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
       <Form.Control
         type="search"
         placeholder="Search Songs/Artists"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
-      ></Form.Control>
+      />
+      <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}></div>
     </Container>
   );
 }
