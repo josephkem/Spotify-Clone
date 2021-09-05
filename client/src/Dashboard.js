e854fe7978effb0bd2ab7f4ac93f4afc48ee7393
josephkem <josephkemoy@gmail.com>
2021-09-05 15:19:11 -0400

    Added a function that gets various pieces of data from the search'

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -21,7 +21,23 @@ export default function Dashboard({ code }) {
     if (!search) return setSearchResults([]);
     if (!accessToken) return;
     spotifyApi.searchTracks(search).then((res) => {
-      console.log(res.body.tracks.items);
+      setSearchResults(
+        res.body.tracks.items.map((track) => {
+          const smallestAlbumImage = track.album.images.reduce(
+            (smallest, image) => {
+              if (image.height < smallest.height) return image;
+              return smallest;
+            },
+            track.album.images[0]
+          );
+          return {
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+            albumUrl: smallestAlbumimage.url,
+          };
+        })
+      );
     });
   }, [search, accessToken]);
 
